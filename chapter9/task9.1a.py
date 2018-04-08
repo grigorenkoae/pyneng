debb4cd681559589b3be855b4bd7f148f4399b79
grigorenkoae <grigorenkoae@gmail.com>
2018-04-08 22:35:16 +0300

    task 9.1 + 9.1a + 9.1b

---
+++ chapter9/task9.1a.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from pprint import pprint
+
+def generate_access_config(access,psecurity=False):
+    '''
+    access - словарь access-портов,
+    для которых необходимо сгенерировать конфигурацию, вида:
+        { 'FastEthernet0/12':10,
+        'FastEthernet0/14':11,
+        'FastEthernet0/16':17}
+
+    Возвращает список всех портов в режиме access
+    с конфигурацией на основе шаблона
+    '''
+    access_template = [ 'switchport mode access',
+                        'switchport access vlan {}',
+                        'switchport nonegotiate',
+                        'spanning-tree portfast',
+                        'spanning-tree bpduguard enable']
+    
+    port_security = [   'switchport port-security maximum 2',
+                        'switchport port-security violation restrict',
+                        'switchport port-security']
+    
+    conf = []
+
+    for key,val in access.items():
+        conf.append(' '.join(['interface',key]))
+        for line in access_template:
+            if 'vlan' in line:
+                conf.append(line.format(val))
+            else:
+                conf.append(line)
+        if psecurity == True:
+            conf.extend(port_security)
+    
+    return conf
+
+
+access_dict = { 'FastEthernet0/12':10,
+'FastEthernet0/14':11,
+'FastEthernet0/16':17,
+'FastEthernet0/17':150 }
+
+
+pprint (generate_access_config(access_dict, psecurity=True))
+
+#pprint (generate_access_config(access_dict))
